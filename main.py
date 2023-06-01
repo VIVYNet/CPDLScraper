91ca37a7c23ec9a2d79837a2f860744747e97660
kmlkmlkmlkmlkml <mkang45@asu.edu>
2023-06-01 14:04:00 +0800

    Bug fixed

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ PASSWORD = login_info["password"]
 DOMAIN = "https://www.cpdl.org"
 TR_KEYS = ["text", "linktext", "translation"]
 
+T_AND_T = ['text', 'original text and translations', 'text and translations', 'texts and translations']
+
 # Connect to MongoDB, whilst checking if the login file specified localhost
 if ADDRESS.lower() == "localhost":
     client = pymongo.MongoClient(f"mongodb://localhost/")
@@ -37,49 +39,157 @@ else:
 db = client['VIVY']
 col = db['cpdlCOL']
 
+def get_page_by_id(page_id):
+    url = f'https://cpdl.org/wiki/api.php?action=query&prop=info&inprop=url&pageids={page_id}&format=json'
+    with urlopen(url) as f:
+        d = json.loads(f.read())['query']['pages'][f'{page_id}']
+    assert 'missing' not in d
+    return d
+
+def redirect(title):
+    url = f'https://cpdl.org/wiki/api.php?action=query&titles={title}&redirects=1&format=json'
+    with urlopen(url) as f:
+        page = list(json.loads(f.read())['query']['pages'].values())[0]
+    assert 'missing' not in page
+    page_id = page['pageid']
+    return get_page_by_id(page_id)
+
+# parse function for `text and translations`
+def parse_text(wikitext_raw):
+    out = {}
+    # Parse JSON response for the text with wikitext parser
+    text = mwparserfromhell.parse(wikitext_raw)
+    
+    # Get templates from parsed wikitext
+    templates = text.filter_templates(recursive=False)
+
+    # Iterate through the templates
+    for index, item in enumerate(templates):
+        # Check if the iterate item is one of the three allowed keys
+        key = str(item.name.lower())
+        if key in TR_KEYS:
+            # Add translation to translation dictionary
+            if key not in out: out[key] = []
+            out[key].extend([str(i) for i in item.params])
+    
+    return out
+
 # Scrape function
-def scrape(link):
+def scrape(link, text_only=False):
+    '''
+        params:
+            link        
+
+            text_only   
+                `greneral_data` won't be computed and will returns `None` if `text_only` is `True`. 
+        returns:
+            Tuple (`greneral_data` | `None`, `text_and_translation`)
+    '''
     # Try
-    try:        
+    try:
+        
         # Variable declaration
         gen_info = {}
         translations = {}
         downloads = {}
+
+        # Fetch title of the page
+        title = link.replace("/wiki/index.php/", "")
         
+        urls = redirect(title)
+        url, editurl = urls['fullurl'], urls['editurl']
+        
+        # Print link
+        # print('-', link)
+
         # Get page info and bs4 instance based on given link
-        link_html = urlopen(DOMAIN + link)
+        link_html = urlopen(url)
         page_soup = BeautifulSoup(link_html, "lxml")
         
         # Make a UUID for the link
         id = uuid.uuid4().hex
         
-        # Fetch title of the page
-        title = link.replace("/wiki/index.php/", "")
-        
-        # Print link
-        print(link)
-        
         # Get page's source page
-        wikitext_soup = BeautifulSoup(urlopen(f"https://www.cpdl.org/wiki/index.php?title={title}&action=edit"), "lxml")
+        wikitext_soup = BeautifulSoup(urlopen(editurl), "lxml")
         
         # Get the text of wikitext
         general_wikitext = wikitextparser.parse(wikitext_soup.find("textarea", attrs={"id": "wpTextbox1"}).text)
         
         # Generate a list of titles
-        indexes = [str(item.title).lower() for item in general_wikitext.sections]
+        indexes = [str(item.title).lower() for item in general_wikitext.get_sections(level=2)]
+        #
+        #   TRANSLATION
+        #
+        # Get wikitext data from given page
+        wikitext_raw = None
+        for t_and_t in T_AND_T:
+            if t_and_t in indexes:
+                wikitext_raw = str(general_wikitext.get_sections(level=2)[indexes.index(t_and_t)])
+        if wikitext_raw is None: raise f'No text or translations in the page: {link}'
         
-        # try:
-        #     a = indexes.index("general information")
-        # except:
-        #     input(indexes) 
+        translations = parse_text(wikitext_raw)
         
+        if 'linktext' in translations:
+            del translations['linktext']
+            # find the main `div`
+            main_content = page_soup.find(id='mw-content-text').find(class_='mw-parser-output')
+
+            # split it by `h2`
+            content_dict = {}
+            _current_key = ''
+            for child in main_content.children:
+                if len(child.text.strip()) == 0: continue
+                if child.name == 'h2':
+                    _current_key = child.text.strip().lower()
+                else:
+                    if _current_key not in content_dict:
+                        content_dict[_current_key] = []
+                    content_dict[_current_key].append(child)
+
+            # get the soup object of text and translations
+            translations_soups = None
+            for t_and_t in T_AND_T:
+                if t_and_t in content_dict: 
+                    translations_soups = content_dict[t_and_t]
+            assert translations_soups is not None
+
+            # get the all `a`
+            links = []
+            for translations_soup in translations_soups:
+                a_list = translations_soup.find_all('a')
+                for a in a_list:
+                    flag = True
+                    if '(page does not exist)' in a['title']: 
+                        flag = False
+                    if flag: links.append(a['href'])
+            
+            for lk in links:
+                # scrape text in the links
+                _, translations_ = scrape(lk, text_only=True)
+                # append them into `translations`
+                for key in TR_KEYS:
+                    if key not in translations: translations[key] = []
+                    translations[key].extend(translations_.get(key, []))
+
+            # delete useless keys
+            keys = list(translations.keys())
+            for key in keys:
+                if len(translations[key]) == 0:
+                    del translations[key]
+        
+        # Sleep for a bit
+        time.sleep(1)
+
+        if text_only:
+            return None, translations
+                
         #
         #   GENERAL INFORMATION
         #
         
         # Get wikitext data from given page
-        wikitext_raw = str(general_wikitext.sections[indexes.index("general information")])
-        
+        wikitext_raw = str(general_wikitext.get_sections(level=2)[indexes.index("general information")])
+
         # Parse JSON response for the text with wikitext parser
         text = mwparserfromhell.parse(wikitext_raw)
         
@@ -105,40 +215,7 @@ def scrape(link):
                                                  .replace("''","")
                                                  .replace("[[","")
                                                  .replace("]]","")) for i in item.params])]
-            
-            # print(item)
-            # print(f'{index} {item.name} - {", ".join([information_parse(str(i)) for i in item.params])}')
-            # print()
-        
-        
-        #
-        #   TRANSLATION
-        #
-        
-        # Try:
-        try:
-            # Get wikitext data from given page
-            wikitext_raw = str(general_wikitext.sections[indexes.index("original text and translations")])
-        
-        # Except
-        except:
-            # Get wikitext data from given page
-            wikitext_raw = str(general_wikitext.sections[indexes.index("text and translations")])
-        
-        # Parse JSON response for the text with wikitext parser
-        text = mwparserfromhell.parse(wikitext_raw)
-        
-        # Get templates from parsed wikitext
-        templates = text.filter_templates(recursive=False)
-        
-        # Iterate through the templates
-        for index, item in enumerate(templates):
-            # Check if the iterate item is one of the three allowed keys
-            if item.name.lower() in TR_KEYS:
-                # Add translation to translation dictionary
-                translations[str(item.name.lower())] = [str(i) for i in item.params]
-                
-        
+
         #
         #   DOWNLOAD LINKS
         #
@@ -164,43 +241,43 @@ def scrape(link):
         #
         #   DATA COMPILING
         #
-        
-        # try:
-        #     a = gen_info['composer'][0]
-        # except:
-        #     input(json.dumps(gen_info, indent=3)) 
-        
         # Construct a dictionary for the information to be inserted into DB
-        insert_data = {
-            "_id": uuid.uuid4().hex,
+        data = {
+            "_id": id,
             "link": link,
             "title": title,
             "composer": gen_info['composer'][0],
             "general_information": gen_info,
-            "translations": translations,
             "download_links": downloads
         }
+        return data, translations
+    # Except
+    except Exception as e:
+        # Print status
+        print(f"One link not scraped >>> {link}")
         
-        # # input("done")
-        # input(json.dumps(gen_info, indent=3))        
+        traceback.print_exc()
+        # Log error
+        with open(f'logs/{uuid.uuid4().hex}.txt', 'w+') as file:
+            traceback.print_exc(file=file)
         
+def scrape_song(link):
+    print(link)
+    insert_data, translations = scrape(link)
+    # Try
+    try:
+        # Construct a dictionary for the information to be inserted into DB
+        insert_data['translations'] = translations
         # Insert to collection 
         col.insert_one(insert_data)
-        
-        # Sleep for a bit
-        time.sleep(1)
-    
     # Except
     except Exception as e:
         # Print status
         print(f"One link not added >>> {link}")
-        
         # Log error
-        with open(f'logs/{uuid.uuid4().hex}.txt', 'w') as file:
+        with open(f'logs/{uuid.uuid4().hex}.txt', 'w+') as file:
             traceback.print_exc(file=file)
-        
-        # # Pause 
-        # input()
+
 
 # Recursive wikitext parser
 def information_parse(value):
@@ -225,6 +302,7 @@ def information_parse(value):
 
     # Else, return the given value
     return value
+
 
 # Main thread
 if __name__ == '__main__':
@@ -265,7 +343,7 @@ if __name__ == '__main__':
                 
         # Run scraping method
         with concurrent.futures.ProcessPoolExecutor() as executor:
-            _ = [executor.submit(scrape, link) for link in list_of_links]
+            _ = [executor.submit(scrape_song, link) for link in set(list_of_links)]
 
         # Print next page
         print(f"-= NEXT PAGE {page}/4000 =-")
